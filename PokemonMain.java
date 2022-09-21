5cd56a0ff988e566245a26f6f4462a3151f48264
gusbomjod <gusbomjod@student.gu.se>
2022-09-21 14:43:55 +0200

    Commit

--- PokemonMain.java
+++ PokemonMain.java
@@ -1,7 +1,7 @@
 public class PokemonMain{
 
     public static void main(String[] args){
-        System.out.println("Hello");
-    }
 
+        System.out.println("Hello pokemonGuyMan!");
+    }
 }
