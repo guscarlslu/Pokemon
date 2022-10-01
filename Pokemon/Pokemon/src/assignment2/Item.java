ff8823e2f286b5b5095bd547330c1aeded81f97a
guscarlslu <113991293+guscarlslu@users.noreply.github.com>
2022-10-01 16:55:26 +0200

    Add files via upload

---
+++ Pokemon/Pokemon/src/assignment2/Item.java
@@ -0,0 +1,28 @@
+package assignment2;
+public class Item {
+    //Each item has a name, a healing power value (integer) and a weight (double)
+    final String itemName;
+    final int healPower;
+    final double itemWeight;
+
+    public Item (String itemName, int healPower, double itemWeight){
+        this.itemName = itemName;
+        this.healPower = healPower;
+        this.itemWeight = itemWeight;
+    }
+
+    public String getItemName() {return this.itemName;}
+
+    public int getHealPower() {
+        return this.healPower;
+    }
+
+    public double getItemWeight(){
+        return itemWeight;
+    }
+
+    public String fullItem(){
+        String fullItem = this.itemName + this.healPower + this.itemWeight;
+        return fullItem;
+    }
+}
